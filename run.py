71207487c90953f7cb5532945fdf7e70ff01d0e6
adrianashymoniak <adrianashymoniak@gmail.com>
2023-07-18 10:56:22 +0300

    feat: add link to the app service

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
 log_file = "Your Results"
 
 port = os.environ.get("PORT") or 8000
-host_url = os.environ.get("HOST") or "0.0.0.0:8000"
+host_url = os.environ.get("HOST") or "cold-mail-generator.azurewebsites.net"
 
 app = FastAPI(title='Cold Mail Generator Demo')
 
