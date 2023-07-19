51522ac450ecc15b41cbc73be326e0fe78ff061d
adrianashymoniak <adrianashymoniak@gmail.com>
2023-07-19 18:35:03 +0300

    feat: add error exception

--- run.py
+++ run.py
@@ -2,10 +2,13 @@ import asyncio
 import os
 from pathlib import Path
 
+import aiohttp
 import openai
 import uvicorn
+from apify_client._errors import ApifyApiError
 from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
+from openai.error import AuthenticationError
 
 from cold_mail_generator import Analyze
 
@@ -51,30 +54,44 @@ async def websocket_endpoint(websocket: WebSocket):
             is_url = True if website_url is not None and is_http else False
             is_openai_key = True if openai_key is not None and '-' in openai_key else False
             if is_url and is_openai_key and apify_key:
-                await websocket.send_text(
-                    "Details saved. Please do not reload the page!"
-                )
-                await asyncio.sleep(0.01)
-                await websocket.send_text("Set OpenAI secret key!")
-                await asyncio.sleep(0.01)
-                openai.api_key = openai_key
-                os.environ['OPENAI_API_KEY'] = openai_key
-                await websocket.send_text("Set Apify secret key!")
-                await asyncio.sleep(0.01)
-                openai.api_key = openai_key
-                os.environ['APIFY_API_TOKEN'] = apify_key
+                async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(
+                        verify_ssl=False)) as aio_session:
+                    async with aio_session.get(website_url) as website:
+                        try:
+                            if website.status == 200:
+                                await websocket.send_text(
+                                    "Details saved. Please do not reload the page!"
+                                )
+                                await asyncio.sleep(0.01)
+                                await websocket.send_text("Set OpenAI secret key!")
+                                await asyncio.sleep(0.01)
+                                openai.api_key = openai_key
+                                os.environ['OPENAI_API_KEY'] = openai_key
+                                await websocket.send_text("Set Apify secret key!")
+                                await asyncio.sleep(0.01)
+                                openai.api_key = openai_key
+                                os.environ['APIFY_API_TOKEN'] = apify_key
 
-                await websocket.send_text("Analyzing...")
-                await asyncio.sleep(0.01)
+                                await websocket.send_text("Analyzing...")
+                                await asyncio.sleep(0.01)
 
-                analyze = Analyze(website_url)
-                await analyze.get_email(websocket)
-                await asyncio.sleep(0.01)
-                await websocket.send_text("Email is generated")
+                                analyze = Analyze(website_url)
+                                await analyze.get_email(websocket)
+                                await asyncio.sleep(0.01)
+                                await websocket.send_text("Email is generated")
 
+                            else:
+                                await websocket.send_text("Please check provided details")
+                                await asyncio.sleep(0.01)
+                        except (AuthenticationError, ApifyApiError) as e:
+                            await websocket.send_text(f"<pre style='white-space: "
+                                                      f"pre-wrap; color:red;'"
+                                                      f">{e}</pre>")
+                            await asyncio.sleep(0.01)
             else:
                 await websocket.send_text("Please check provided details")
                 await asyncio.sleep(0.01)
+
             await websocket.send_text('Analyzing completed.')
             await asyncio.sleep(0.01)
 
